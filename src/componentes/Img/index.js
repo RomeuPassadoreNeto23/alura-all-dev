62db340361cc2f34c12f9ec72539752a4d4c3f3a
Romeu Passadore Neto <passadore.neto@outlook.com>
2024-07-02 16:11:30 -0300

    Campo para selecionar a linguagem

--- src/componentes/Img/index.js
+++ src/componentes/Img/index.js
@@ -3,4 +3,6 @@ import styled from "styled-components";
  export const Img = styled.img`
       width:${props => props.largura || '9px'};
       height:${props => props.altura || '4.5px'};
+      position: relative;
+      right: 20px;
 `
